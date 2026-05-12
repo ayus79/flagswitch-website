324941005373715346d964de836a4394eb390eeb
ayus79 <ayush@simprosys.com>
2026-05-12 12:32:11 +0530

    Update Astro configuration and remove GitHub Actions deployment workflow: change site URL for deployment and delete obsolete deploy.yml file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://ayus79.github.io',
-  base: '/flagswitch-website/',
+  site: 'https://flagswitch.ayushcon2000.workers.dev',
+  // base: '/flagswitch-website/',
   integrations: [tailwind()],
   output: 'static',
 });
